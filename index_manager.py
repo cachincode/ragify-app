6e2887667f2510fdf8c2b6c8c5f65d614c2c2015
código_cachín <cachincode@gmail.com>
2024-10-20 16:23:09 -0600

    feat: use mongodb as index store instead of a default one

--- index_manager.py
+++ index_manager.py
@@ -1,31 +1,57 @@
+import os
 from llama_index.core import (
     StorageContext,
     VectorStoreIndex,
     SimpleDirectoryReader,
     Settings
 )
+from llama_index.core import load_index_from_storage
 from llama_index.core.base.base_query_engine import BaseQueryEngine
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.llms.ollama import Ollama
 
 from utils.vector_database import build_chroma_vector_store
+from utils.index_store import build_index_store, get_existing_indexes
+
+
+mongo_index_id = os.getenv("MONGO_INDEX_ID", "")
 
 Settings.llm = Ollama(model="llama3.1", request_time=360.0)
 Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text")
 
 vector_store, vector_store_exists = build_chroma_vector_store()
+index_store = build_index_store()
 
 storage_context = StorageContext.from_defaults(
     vector_store = vector_store,
+    index_store=index_store
 )
 
+
 def initialize_index() -> BaseQueryEngine:
-    if vector_store_exists:
-        index = VectorStoreIndex.from_vector_store(vector_store=vector_store, storage_context=storage_context)
+    """
+    """
+    index = None
+
+    existing_indexes = get_existing_indexes()
+    if len(existing_indexes) > 0:
+        print("Loading existing index...")
+        index = load_index_from_storage(storage_context=storage_context, index_id=mongo_index_id)
     else:
-        documents = SimpleDirectoryReader(input_dir="./data").load_data(show_progress=True)
-        index = VectorStoreIndex.from_documents(documents=documents, storage_context=storage_context)
+        print("Creating index...")
+        index = build_vector_index()
 
     query_engine = index.as_query_engine(similarity_top_k=5)
 
     return query_engine
+
+def build_vector_index():
+    """
+    """
+    documents = SimpleDirectoryReader(input_dir="./data").load_data(show_progress=True)
+
+    index = VectorStoreIndex.from_documents(documents=documents, storage_context=storage_context)
+
+    index.set_index_id(mongo_index_id)
+
+    return index
